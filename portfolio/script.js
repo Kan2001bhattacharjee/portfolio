6b7cf24316ab9ebe24d7d44346fda956cbaaede2
Kan2001bhattacharjee <104816567+Kan2001bhattacharjee@users.noreply.github.com>
2024-03-04 18:33:05 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,2 @@
+document.getElementById('imge1').addEventListener('click',function(){window.open('https://flourishing-cassata-cad338.netlify.app','_blank')})
+document.getElementById('imge2').addEventListener('click',function(){window.open('https://remarkable-mermaid-afba2b.netlify.app','_blank')})
